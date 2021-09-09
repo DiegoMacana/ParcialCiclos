2ac48397c80d786c1ce50db30142330ed934f29a
Diego Macana <diego.macana@mail.escuelaing.edu.co>
2021-09-09 10:04:03 -0500

    parcial

--- src/main/java/edu/eci/cvds/ecibook/service/BookSaleService.java
+++ src/main/java/edu/eci/cvds/ecibook/service/BookSaleService.java
@@ -17,11 +17,47 @@ public class BookSaleService {
 	 * @return The amount to be charged to the client
 	 */
 	public BigDecimal calculateCost(final BigDecimal bookCategory, final DiscountCategory discountCategory) {
-
-		return BigDecimal.TEN;
+                                        BigDecimal precio= getPrice(bookCategory);
+                                        switch (discountCategory){
+                                            case EMPLOYEE:
+                                                 BigDecimal tarifa=   new BigDecimal(0.1);
+                                                return precio.subtract(precio.multiply(tarifa));
+                                            case STUDENT:
+                                                BigDecimal tarifa2=   new BigDecimal(0.2);
+                                                return precio.subtract(precio.multiply(tarifa2));
+                                              case OTHER:
+                                                  BigDecimal tarifa3=   new BigDecimal(1);
+                                                return precio.subtract(precio.multiply(tarifa3));
+                                              default:
+                                                  throw new IllegalStateException("Category dont find");
+                                        }	
 	}
-
+                    public BigDecimal getPrice(final BigDecimal bookCategory) {
+                               BigDecimal uno =  new BigDecimal(1);
+                               BigDecimal dies =  new BigDecimal(1);
+                               BigDecimal once =  new BigDecimal(1);
+                               BigDecimal veinte =  new BigDecimal(1);
+                               BigDecimal ventiuno =  new BigDecimal(1);
+                               BigDecimal treinta =  new BigDecimal(1);
+                               
+                               BigDecimal p1 =  new BigDecimal(25000);
+                               BigDecimal p2 =  new BigDecimal(50000);
+                               BigDecimal p3 =  new BigDecimal(100000);
+                               
+                              if (bookCategory.compareTo(uno) >=0 && bookCategory.compareTo(dies) <=0){
+                                  return p1;
+                                } 
+                              if (bookCategory.compareTo(once) >=0 && bookCategory.compareTo(veinte) <=0){
+                                  return p2;
+                                } 
+                               if (bookCategory.compareTo(ventiuno) >=0 && bookCategory.compareTo(treinta) <=0){
+                                  return p2;
+                                } 
+                               else{ return BigDecimal.ZERO;}
+                    }
+                        
 	/*
+                    
 	 * Tip: Siempre que se desee realizar cálculos matemáticos de alta precisión (por ejemplo para temas de dinero)
 	 * es mucho más confiable usar el tipo de dato BigDecimal, pues soporta un alto rango de número, decimales y
 	 * tiene mayor precisión en los cálculos. <br />
--- src/test/java/edu/eci/cvds/ecibook/service/BookSaleServiceTest.java
+++ src/test/java/edu/eci/cvds/ecibook/service/BookSaleServiceTest.java
@@ -14,10 +14,23 @@ public class BookSaleServiceTest {
 	 * The class under test.
 	 */
 	private BookSaleService service = new BookSaleService();
-
+                    private DiscountCategory discountCategory=new DiscountCategory();
+        
+                     @Test
+	public void test1() {
+                                        BigDecimal califica =  new BigDecimal(1);
+                                        BigDecimal rta =  new BigDecimal(1);
+		Assert.assertEquals(rta,  service.calculateCost(califica, discountCategory.OTHER));
+	}
+        
+                     @Test
+	public void test2() {
+                                        BigDecimal rta =  new BigDecimal(1);
+		Assert.assertEquals(rta,  service.calculateCost(null, null));
+	}
 	@Test
-	public void test() {
-
-		Assert.assertEquals(BigDecimal.ZERO,  service.calculateCost(null, null));
+	public void test3() {
+                                        BigDecimal rta =  new BigDecimal(1);
+		Assert.assertEquals(rta,  service.calculateCost(null, null));
 	}
 }
